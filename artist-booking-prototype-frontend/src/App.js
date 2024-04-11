f39638621893e49c89dcec3f1f76f99f4fe3349a
Neq <theskaterfriend@gmail.com>
2024-04-11 20:30:23 +0200

    * Logo and layout changes

--- artist-booking-prototype-frontend/src/App.js
+++ artist-booking-prototype-frontend/src/App.js
@@ -31,8 +31,8 @@ function App() {
     <div>
 <nav class="navbar navbar-light bg-light p-3">
         <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
-            <a class="navbar-brand" href="#">
-                WoF Artist Booking
+            <a class="navbar-brand" href="#">                
+                <img src="/WoF_logo.png" class="wof-logo" />
             </a>
             <button class="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
