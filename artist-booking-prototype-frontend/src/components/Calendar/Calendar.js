9aa0fce5605f5328f428701f2553d91e938f7d8a
Neq <theskaterfriend@gmail.com>
2024-04-12 21:18:45 +0200

    * Add possibility to give events a background-color, because of changes to the structure how event work in the prototype

--- artist-booking-prototype-frontend/src/components/Calendar/Calendar.js
+++ artist-booking-prototype-frontend/src/components/Calendar/Calendar.js
@@ -1,34 +1,95 @@
 // components/Calendar/Calendar.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 export default function Calendar() {
 
-    const [events, setEvents] = useState([]);
+    const [artistEvents, setArtistEvents] = useState({});
+    const calendarRef = useRef(null);
 
     useEffect(() => {
-        fetchEvents();
+        fetchArtistEvents();        
       }, []);
 
-    const fetchEvents = async () => {
+    const fetchArtistEvents = async () => {
         try {
             const response = await axios.get('http://localhost:8080/events');
-            setEvents(response.data);
+            //setEvents(response.data);
+            setArtistEvents([                  
+                  {
+                    events: response.data,
+                    color: 'black',     
+                    textColor: 'green',
+                    className: 'asdf'
+                  }
+                ]);
+            /*setArtistEvents({
+
+                eventSources: [
+              
+                  // your event source
+                  {
+                    events: [ // put the array in the `events` property
+                      {
+                        title  : 'event1',
+                        start  : '2024-01-01'
+                      },
+                      {
+                        title  : 'event2',
+                        start  : '2024-01-05',
+                        end    : '2024-01-07'
+                      },
+                      {
+                        title  : 'event3',
+                        start  : '2024-01-09T12:30:00',
+                      }
+                    ],
+                    color: 'black',     // an option!
+                    textColor: 'yellow' // an option!
+                  }
+              
+                  // any other event sources...
+              
+                ]
+              
+              });*/
+            //fetchCalendarEvents();
+            //console.log("ArtistEvents: "+response.data + " artistEvents: "+artistEvents);
         } catch (error) {
             console.error('Error fetching tasks:', error);
-        }
+        }        
     };
+
+    const fetchCalendarEvents = () => {        
+        
+        console.log("calendarEvents - artistEvents: "+artistEvents);
+        const calendarApi = calendarRef.current.getApi();
+        calendarApi.addEventSource([artistEvents]);
+        calendarApi.refetchEvents();
+    }
+
+    function renderEventContent(eventInfo) {
+        return(
+          <>
+            <div style={{backgroundColor: 'red'}}>
+            <b>{eventInfo.timeText}</b>
+            <i>{eventInfo.event.title} {eventInfo.event.id}</i>
+            </div>
+          </>
+        )
+      }
 
     return (
         <FullCalendar
         plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-        events={
-            events
-        }
-
+        ref={calendarRef}
+        eventSources={artistEvents}
+        initialView="dayGridMonth"  
+        eventContent={renderEventContent}      
         />
     )
 }
+
+
